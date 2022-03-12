6f95136971c459fb48254e6f55512654dca98384
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-03-12 21:04:36 +0530

    Manipulating-variables
    https://www.youtube.com/watch?v=-gbo9iZPOjs&t=18s

---
+++ Validation/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  shared_credentials_file = "C:/Users/raj13/.aws/credentials"
+  region  = var.location
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServerInstance"
+  }
+}
